8308b7fe5fffbe9f364022de6ab29697f8d8bdd8
Jack Corless <jackcorless91@gmail.com>
2024-09-25 10:17:02 +1000

    basic inventory class created

--- classes.py
+++ classes.py
@@ -1,3 +1,5 @@
+from app import name
+
 class start_new_game():
   pass
 
@@ -9,3 +11,24 @@ class reset_game():
 
 class basement_corridor():
   pass
+
+
+class player_inventory:
+  def __init__(self):
+    self.items = []
+  
+  def add_item(self, item):
+    self.items.append(item)
+    print(f"{item} has been added to your inventory")
+
+  def remove_item(self, item):
+    if item in self.items:
+      self.items.remove(item)
+      print(f"{item} has been removed from your inventory")
+
+  def view_inventory(self):
+    if self.items:
+      print("{name} contains: ")
+    
+
+  
